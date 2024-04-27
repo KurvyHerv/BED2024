6bc37e6875d18f930c8dacd9e4d8c616edf1293c
Hervin Sie <hervsie@gmail.com>
2024-04-27 19:56:52 +0800

    week1 prac1 commit

---
+++ week1/app.js
@@ -0,0 +1,27 @@
+const path = require('node:path');
+const fs = require('node:fs')
+const note = '/BED/BED2024/note.txt';
+const content = 'hello world'
+const chalk = require('chalk');
+
+path.dirname(note);
+path.basename(note);
+path.extname(note);
+
+fs.writeFile('note.txt', content, err => {
+    if (err) {
+        console.error(err);
+    } else {
+
+    }
+});
+
+fs.readFile('note.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
+
+console.log(chalk.yellow('hi!'));
